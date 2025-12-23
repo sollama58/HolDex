b9bda632aac0464000b4dbc3347517a6024c3edf
Sollama <rtrdgcr@gmail.com>
2025-12-23 17:16:51 -0500

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -8,5 +8,5 @@ module.exports = {
     CORS_ORIGINS: process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : '*',
     METADATA_UPDATE_INTERVAL: parseInt(process.env.METADATA_UPDATE_INTERVAL) || 60000,
     HOLDER_SCAN_INTERVAL: parseInt(process.env.HOLDER_SCAN_INTERVAL) || 300000,
-    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'asdf'
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD
 };
