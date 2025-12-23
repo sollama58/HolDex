18f2e85681a850622d8f7a7e16fe1e8a9965c993
Sollama <rtrdgcr@gmail.com>
2025-12-23 17:16:40 -0500

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -8,6 +8,5 @@ module.exports = {
     CORS_ORIGINS: process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : '*',
     METADATA_UPDATE_INTERVAL: parseInt(process.env.METADATA_UPDATE_INTERVAL) || 60000,
     HOLDER_SCAN_INTERVAL: parseInt(process.env.HOLDER_SCAN_INTERVAL) || 300000,
-    // Default password for the new admin panel
-    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'admin123'
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'asdf'
 };
